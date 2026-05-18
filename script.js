cec7e1fdca634ae2a486b1c0fb5dba81ad7c86e5
AV <sanaoloviee@gmail.com>
2026-05-18 19:44:40 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,13 +5,3 @@ function showPage(pageId) {
     target.classList.remove('hidden');
   }
 }
-
-function togglePassword(inputId) {
-  const input = document.getElementById(inputId);
-  if (input.type === "password") {
-    input.type = "text";
-  } else {
-    input.type = "password";
-  }
-}
-
